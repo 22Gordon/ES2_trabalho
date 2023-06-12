2d8bd0922f6b95d5992cd2e27bb3df54dd6cf547
EduardoLousinha <edulousinha@gmail.com>
2023-06-12 11:43:53 +0100

    Project and user task controllers.

--- Backend/Controllers/ProjectsController.cs
+++ Backend/Controllers/ProjectsController.cs
@@ -17,6 +17,7 @@ namespace Backend.Controllers
         }
         
         // GET: api/Projects
+        // List all projects
         [HttpGet]
         public async Task<ActionResult<IEnumerable<dynamic>>> getProjects()
         {
@@ -37,6 +38,66 @@ namespace Backend.Controllers
                 p.Freelancers,
                 p.UserTasks
             }).ToListAsync();
+        }
+        
+        // GET api/projects/{id}
+        // List specific project, based on id
+        [HttpGet("{id}")]
+        public IActionResult GetProjectById(Guid id)
+        {
+            var project = _context.Projects.FirstOrDefault(p => p.Projectid == id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+            return Ok(project);
+        }
+        
+        // PUT api/projects/{id}
+        // Update db specific project, based on id
+        [HttpPut("{id}")]
+        public IActionResult UpdateProject(Guid id, Project updatedProject)
+        {
+            var project = _context.Projects.FirstOrDefault(p => p.Projectid == id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            // Update project properties
+            project.Name = updatedProject.Name;
+            project.Projectleaderid = updatedProject.Projectleaderid;
+            project.Clientid = updatedProject.Clientid;
+            project.Pricehour = updatedProject.Pricehour;
+
+            _context.SaveChanges();
+            return NoContent();
+        }
+        
+        // POST api/projects
+        // Creates a db entity of a project
+        [HttpPost]
+        public IActionResult CreateProject(Project project)
+        {
+            _context.Projects.Add(project);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetProjectById), new { id = project.Projectid }, project);
+        }
+        
+        // DELETE api/projects/{id}
+        // Deletes an entity from the database
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProject(Guid id)
+        {
+            var project = _context.Projects.FirstOrDefault(p => p.Projectid == id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            _context.Projects.Remove(project);
+            _context.SaveChanges();
+            return NoContent();
         }
     }
 }
---
+++ Backend/Controllers/UserTasksController.cs
@@ -0,0 +1,89 @@
+﻿using BusinessLogic.Context;
+using BusinessLogic.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserTasksController : ControllerBase
+    {
+        private readonly MyDbContext _context;
+
+        public UserTasksController(MyDbContext context)
+        {
+            _context = context;
+        }
+        
+        // GET: api/usertasks
+        // List all user tasks
+        [HttpGet]
+        public IActionResult GetAllUserTasks()
+        {
+            var tasks = _context.UserTasks.ToList();
+            return Ok(tasks);
+        }
+        
+        // GET api/usertasks/{id}
+        // List specific user tasks, based on id
+        [HttpGet("{id}")]
+        public IActionResult GetUserTaskById(Guid id)
+        {
+            var task = _context.UserTasks.FirstOrDefault(t => t.Taskid == id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+            return Ok(task);
+        }
+        
+        // PUT api/usertasks/{id}
+        // Update db specific user task, based on id
+        [HttpPut("{id}")]
+        public IActionResult UpdateUserTask(Guid id, UserTask updatedTask)
+        {
+            var task = _context.UserTasks.FirstOrDefault(t => t.Taskid == id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            // Update task properties
+            task.Projectid = updatedTask.Projectid;
+            task.Freelancerid = updatedTask.Freelancerid;
+            task.Startdate = updatedTask.Startdate;
+            task.Enddate = updatedTask.Enddate;
+            task.Pricehour = updatedTask.Pricehour;
+
+            _context.SaveChanges();
+            return NoContent();
+        }
+        
+        // POST api/usertasks
+        // Creates a db entity of a UserTask
+        [HttpPost]
+        public IActionResult CreateUserTask(UserTask task)
+        {
+            _context.UserTasks.Add(task);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetUserTaskById), new { id = task.Taskid }, task);
+        }
+        
+        // DELETE api/usertasks/{id}
+        // Deletes an entity from the database
+        [HttpDelete("{id}")]
+        public IActionResult DeleteUserTask(Guid id)
+        {
+            var task = _context.UserTasks.FirstOrDefault(t => t.Taskid == id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            _context.UserTasks.Remove(task);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
